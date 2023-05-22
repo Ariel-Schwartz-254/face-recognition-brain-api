835ba443626e11a8ebfbb7d45b6995bb66287a4a
Ariel Schwartz <schwartz254@gmail.com>
2023-05-22 14:59:02 +0300

    Moving API call to the server

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,38 @@
+const {ClarifaiStub, grpc} = require("clarifai-nodejs-grpc");
+
+const stub = ClarifaiStub.grpc();
+
+const metadata = new grpc.Metadata();
+metadata.set("authorization", "Key a25b8a1cc8b743af8681c8ba49f2d59e");
+
+const handleApiCall = (req, res) => {
+    stub.PostModelOutputs(
+        {
+            // This is the model ID of the face detection model. You may use any other public or custom model ID.
+            model_id: "face-detection",
+            inputs: [{data: {image: {url: req.body.input}}}]
+        },
+        metadata,
+        (err, response) => {
+            if (err) {
+                console.log("Error: " + err);
+                return;
+            }
+    
+            if (response.status.code !== 10000) {
+                console.log("Received failed status: " + response.status.description + "\n" + response.status.details);
+                return;
+            }
+    
+            // console.log("Predicted concepts, with confidence values:")
+            // for (const c of response.outputs[0].data.concepts) {
+            //     console.log(c.name + ": " + c.value);
+            // }
+            res.json(response)
+        }
+    ); 
+}
+
 const handleImage = (req, res, db) => {
     const { id } = req.body;
     db('users').where('id', '=', id)
@@ -10,5 +45,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-    handleImage
+    handleImage,
+    handleApiCall
 }
